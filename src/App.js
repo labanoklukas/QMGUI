a0b77a0aebd98b57eaf07df12f9e692d669e087b
labanoklukas <120426805+labanoklukas@users.noreply.github.com>
2024-03-21 21:57:06 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
 import './App.css';
+import { Router, Route, } from 'react-router-dom';
 import WeatherApp from './WeatherApp/WeatherApp.jsx';
+import Forecast from './Forecast/Forecast.jsx';
+import Snow from './Snow/Snow.jsx';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <WeatherApp/>
+    <div className="background">
+      <div className='blurBackdrop'>
+        <Router>
+          <Route exact path = "/" component={WeatherApp} />
+          <Route path = "/forecast" component={Forecast} />
+          <Route path = "/Snow" component={Snow} />
+        </Router>
+      </div>
     </div>
   );
 }
