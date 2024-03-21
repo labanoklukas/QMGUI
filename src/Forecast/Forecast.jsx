997c5adbb7f02dde973c43d856dd0b13c0819d12
labanoklukas <120426805+labanoklukas@users.noreply.github.com>
2024-03-21 21:59:12 +0000

    Update Forecast.jsx

--- src/Forecast/Forecast.jsx
+++ src/Forecast/Forecast.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Forecast.css';
 
 const api_key = "037801e525acc86cf8c189da8468446e";
@@ -8,6 +9,7 @@ const Forecast = () => {
     return (
         <div className='container'>
             <h1>Forecast Page</h1>
+            <Link to="/">Back</Link>
             <p>This is the Forecast page content.</p>
         </div>
 
